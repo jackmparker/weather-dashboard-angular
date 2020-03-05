e44428c84d3a5c8b38f17145a5b0206f6c2677a8
Jack Parker <jackmichaelparker@gmail.com>
2020-03-04 22:36:52 -0700

    Updated environment files.

--- src/app/services/location-search.service.ts
+++ src/app/services/location-search.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationSearchService {
 
+	apiRoot: string = environment.apiRoot;
+
   constructor(private http: HttpClient) { }
 
 	getLocation(query: string) {
-		return this.http.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=0nP9GmS7wqLj3LgOfvA6IxzacxoAgR7y&q=' + query);
+		return this.http.get(this.apiRoot +'/locations/v1/cities/autocomplete?apikey=0nP9GmS7wqLj3LgOfvA6IxzacxoAgR7y&q=' + query);
 	}
 
 	getCurrent(key: string) {
-		return this.http.get('https://dataservice.accuweather.com/currentconditions/v1/' + key + '?apikey=0nP9GmS7wqLj3LgOfvA6IxzacxoAgR7y');
+		return this.http.get(this.apiRoot +'/currentconditions/v1/' + key + '?apikey=0nP9GmS7wqLj3LgOfvA6IxzacxoAgR7y');
 	}
 
 	getForecast(key: string) {
-		return this.http.get('https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + key + '?apikey=0nP9GmS7wqLj3LgOfvA6IxzacxoAgR7y');
+		return this.http.get(this.apiRoot +'/forecasts/v1/hourly/12hour/' + key + '?apikey=0nP9GmS7wqLj3LgOfvA6IxzacxoAgR7y');
 	}
 }
